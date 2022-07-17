b0fee9c2f417a14c307a6013b21242b71bc4855d
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-17 16:01:20 -0700

    Cleaned up unused Imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Nav from "./components/Nav";
-import { trpc } from "../utils/trpc";
-import { useState } from "react";
 
 const Home: NextPage = () => {
   return (
