33c06b5846a1bff417d7d5c104cda10234df8802
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-18 10:14:48 -0700

    Merged Post Router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { UserRouter } from "./User";
+import { PostRouter } from "./Post";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("UserRouter.", UserRouter);
+  .merge("UserRouter.", UserRouter)
+  .merge("PostRouter.", PostRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
