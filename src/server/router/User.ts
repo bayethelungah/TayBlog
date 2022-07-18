59cd26a0e0b4a931b46ba358409c44dc560e2187
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-17 18:23:03 -0700

    Updated create-user mutation to have username field

--- src/server/router/User.ts
+++ src/server/router/User.ts
@@ -6,16 +6,18 @@ import jwt from "jsonwebtoken";
 export const UserRouter = createRouter()
   .mutation("create-user", {
     input: z.object({
-      name: z.string().min(5).max(20),
+      fullName: z.string().min(5).max(20),
       email: z.string().email().min(5),
       password: z.string().min(5).max(20),
+      username: z.string().min(5).max(20),
     }),
     async resolve({ input }) {
-      await prisma.users.create({
+      await prisma.user.create({
         data: {
-          fullname: input.name,
+          fullname: input.fullName,
           email: input.email,
           password: input.password,
+          username: input.username,
         },
       });
     },
@@ -26,7 +28,7 @@ export const UserRouter = createRouter()
       password: z.string().min(5).max(20),
     }),
     async resolve({ input }) {
-      let res = await prisma.users.findUnique({
+      let res = await prisma.user.findUnique({
         where: {
           email: input.email,
         },
