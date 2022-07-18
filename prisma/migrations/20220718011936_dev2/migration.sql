9b0a0c1601131cab130c5f09b98e4e6dd2f709ce
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-17 18:24:24 -0700

    Updated dropped pluralized naming for User

---
+++ prisma/migrations/20220718011936_dev2/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Users";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "username" TEXT NOT NULL DEFAULT 'USER'
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
