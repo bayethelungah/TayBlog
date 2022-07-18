835d79d63fc09fa2a2c4e11829a379c9c78dd917
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-17 18:08:25 -0700

    Created login mutation

--- src/server/router/User.ts
+++ src/server/router/User.ts
@@ -1,20 +1,40 @@
 import { createRouter } from "./context";
 import { z } from "zod";
 import { prisma } from "../db/client";
+import jwt from "jsonwebtoken";
 
-export const UserRouter = createRouter().mutation("create-user", {
-  input: z.object({
-    name: z.string().min(5).max(20),
-    email: z.string().email().min(5),
-    password: z.string().min(5).max(20),
-  }),
-  async resolve({ input }) {
-    await prisma.users.create({
-      data: {
-        fullname: input.name,
-        email: input.email,
-        password: input.password,
-      },
-    });
-  },
-});
+export const UserRouter = createRouter()
+  .mutation("create-user", {
+    input: z.object({
+      name: z.string().min(5).max(20),
+      email: z.string().email().min(5),
+      password: z.string().min(5).max(20),
+    }),
+    async resolve({ input }) {
+      await prisma.users.create({
+        data: {
+          fullname: input.name,
+          email: input.email,
+          password: input.password,
+        },
+      });
+    },
+  })
+  .mutation("login-user", {
+    input: z.object({
+      email: z.string().email().min(5),
+      password: z.string().min(5).max(20),
+    }),
+    async resolve({ input }) {
+      let res = await prisma.users.findUnique({
+        where: {
+          email: input.email,
+        },
+      });
+
+      if (!res) return { token: null };
+      return {
+        token: jwt.sign(input.email, "secretket"),
+      };
+    },
+  });
