35101b388dc7057073fbe3630d782e1045f9d3f2
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-19 13:41:55 -0700

    Updated Nav for when user is logged in

--- src/pages/components/Nav.tsx
+++ src/pages/components/Nav.tsx
@@ -1,34 +1,64 @@
-import React from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
+import { useAppContext } from "../context/state";
 
 type Props = {};
 
 function Nav({}: Props) {
+  const { getUserInfo, logout } = useAppContext();
+
+  console.log(getUserInfo());
+
   return (
-    <nav className="fixed flex justify-around items-center top-0 left-0 w-screen h-10 mt-5">
+    <nav className="fixed flex justify-around items-center top-0 left-0 w-screen h-14 mt-2 shadow-lg ">
       <h1 className="text-2xl">
         {" "}
         <strong>TayBlog</strong>{" "}
       </h1>
       <div className="flex justify-center items-center gap-4">
-        <motion.a className="text-xl duration-300  hover:cursor-pointer hover:text-cyan-800">
+        <motion.a
+          href="/"
+          className="text-xl duration-300  hover:cursor-pointer hover:text-cyan-800"
+        >
+          Home
+        </motion.a>
+        <motion.a
+          href="/posts"
+          className="text-xl duration-300  hover:cursor-pointer hover:text-cyan-800"
+        >
           Posts
         </motion.a>
-        <motion.a className="text-xl duration-300  hover:cursor-pointer hover:text-cyan-800">
-          Categories
-        </motion.a>
-        <motion.a
-          href="/login"
-          className="text-xl duration-300  hover:cursor-pointer btn-outline"
-        >
-          Login
-        </motion.a>
-        <motion.a
-          href="/register"
-          className="text-xl duration-300  hover:cursor-pointer btn"
-        >
-          Sign up
-        </motion.a>
+        {getUserInfo() == null && (
+          <>
+            <motion.a
+              href="/login"
+              className="text-xl duration-300  hover:cursor-pointer btn-outline p-1"
+              style={{ padding: "5px" }}
+            >
+              Login
+            </motion.a>
+            <motion.a
+              href="/register"
+              className="text-xl duration-300  hover:cursor-pointer btn"
+              style={{ padding: "5px" }}
+            >
+              Sign up
+            </motion.a>
+          </>
+        )}
+        {getUserInfo() !== null && (
+          <>
+            <motion.button className="text-xl duration-300  hover:cursor-pointer btn-outline">
+              {getUserInfo()?.username}
+            </motion.button>
+            <motion.button
+              onClick={() => logout()}
+              className="text-xl duration-300  hover:cursor-pointer btn"
+            >
+              Sign Out
+            </motion.button>
+          </>
+        )}
       </div>
     </nav>
   );
