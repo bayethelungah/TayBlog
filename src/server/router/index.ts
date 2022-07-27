f921ebdd278dc9ec5cb72336a6890e711376498d
Bayethe Lungah <bayethelungah@Bayethes-MacBook-Pro.local>
2022-07-27 16:14:48 -0600

    Got rid of User route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,12 +2,10 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { UserRouter } from "./User";
 import { PostRouter } from "./Post";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("UserRouter.", UserRouter)
   .merge("PostRouter.", PostRouter);
 
 // export type definition of API
