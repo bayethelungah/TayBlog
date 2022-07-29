c47e69a6ea567154ed91051f2489f512708b5734
Bayethe Lungah <bayethelungah@bayethes-mbp.attlocal.net>
2022-07-28 21:06:58 -0400

    Create Post mutation

--- src/server/router/Post.ts
+++ src/server/router/Post.ts
@@ -2,10 +2,32 @@ import { createRouter } from "./context";
 import { z } from "zod";
 import { prisma } from "../db/client";
 
-export const PostRouter = createRouter().query("get-all-posts", {
-  input: z.object({ text: z.string().nullish() }).nullish(),
-  async resolve() {
-    const posts = await prisma.post.findMany();
-    return posts;
-  },
-});
+export const PostRouter = createRouter()
+  .query("get-all-posts", {
+    input: z.object({ text: z.string().nullish() }).nullish(),
+    async resolve() {
+      const posts = await prisma.post.findMany();
+      return posts;
+    },
+  })
+  .mutation("create-post", {
+    input: z.object({
+      author: z.string(),
+      body: z.string(),
+      title: z.string(),
+      User: z.object({
+        name: z.string().nullish(),
+        email: z.string().nullish(),
+        image: z.string().nullish(),
+      }),
+    }),
+    async resolve({ input }) {
+      await prisma.post.create({
+        data: {
+          author: input.author,
+          body: input.body,
+          title: input.title,
+        },
+      });
+    },
+  });
