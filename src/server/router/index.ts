00f95ed8cd04498b8ffe337619895ef5ce8f685c
Bayethe Lungah <bayethelungah@bayethes-mbp.attlocal.net>
2022-07-28 21:10:18 -0400

    Merged the User Router to the main router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -3,10 +3,12 @@ import { createRouter } from "./context";
 import superjson from "superjson";
 
 import { PostRouter } from "./Post";
+import { UserRouter } from "./User";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("PostRouter.", PostRouter);
+  .merge("PostRouter.", PostRouter)
+  .merge("UserRouter.", UserRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
