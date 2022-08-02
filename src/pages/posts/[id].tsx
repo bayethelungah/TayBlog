d7faeb1c9801b006ce59f274a6d5e8a8f577a211
Bayethe Lungah <bayethelungah@bayethes-mbp.attlocal.net>
2022-08-01 20:37:26 -0400

    Added Delete Post Mutation and functionality

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,36 +1,16 @@
-import { GetStaticPaths, GetStaticProps } from "next";
-import { prisma } from "../../server/db/client";
-import { ParsedUrlQuery } from "querystring";
+/* eslint-disable react-hooks/rules-of-hooks */
+import { motion } from "framer-motion";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { ParsedUrlQuery } from "querystring";
+import { ReactFragment } from "react";
+import { prisma } from "../../server/db/client";
+import { trpc } from "../../utils/trpc";
 import Nav from "../components/Nav";
 
 interface IParams extends ParsedUrlQuery {
   id: string;
-}
-
-type Post = {
-  body: string;
-  author: string;
-  id: string;
-  title: string;
-};
-
-type PostProps = {
-  post: Post;
-};
-
-export async function getStaticPaths() {
-  const postTitle = [];
-  const users = await prisma.post.findMany();
-  const paths = users.map((user) => {
-    return {
-      params: { id: user.title.replace(" ", "") },
-    };
-  });
-  return {
-    paths,
-    fallback: false,
-  };
 }
 
 type Props = {
@@ -41,27 +21,62 @@ interface Params extends ParsedUrlQuery {
   slug: string;
 }
 
+type Post = {
+  body: string;
+  author: string;
+  id: string;
+  title: string;
+  createdAt: string;
+};
+
+type PostProps = {
+  post: Post;
+};
+
+export async function getStaticPaths() {
+  const users = await prisma.post.findMany();
+  const paths = users.map((user) => {
+    return {
+      params: { id: user.title.replace(" ", ".") },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
 export async function getStaticProps(context: any) {
   const { id } = context.params as Params;
-  const response = await prisma.post.findFirst({
+  const post = await prisma.post.findFirst({
     where: { title: id as string },
   });
-  if (!response) return;
+  if (!post) return;
 
   return {
     props: {
       post: {
-        body: response.body,
-        title: response.title,
-        id: response.id,
-        author: response.author,
+        body: post.body,
+        title: post.title,
+        id: post.id,
+        author: post.author,
+        createdAt: post.createdAt.toUTCString(),
       },
     },
   };
 }
 
-export default function posts({ post }: PostProps) {
+export default function Posts({ post }: PostProps) {
   if (!post) return;
+  const deletePostMutation = trpc.useMutation("PostRouter.delete-post");
+  const date = post.createdAt.split(" ").splice(0, 4).join(" ");
+  const router = useRouter();
+  const { data } = useSession();
+  const handleDelete = (event: React.MouseEvent) => {
+    event.preventDefault();
+    deletePostMutation.mutate({ id: post.id });
+    router.push("/showPosts");
+  };
 
   return (
     <section>
@@ -73,10 +88,20 @@ export default function posts({ post }: PostProps) {
       <Nav />
 
       <main className="flex h-screen justify-center items-center">
-        <div className="flex flex-col justify-center items-center shadow-2xl p-5 rounded-2xl">
-          <h2 className="text-2xl text-cyan-600  ">{post.author}</h2>
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-2xl primary-col">{post.author}</h2>
           <h1 className="text-4xl">{post.title}</h1>
+          {post.author === data?.user?.name && (
+            <button
+              onClick={(event) => handleDelete(event)}
+              className="btn bg-red-500 border-red-500 absolute top-2 right-2 hover:text-black p-1"
+            >
+              Delete
+            </button>
+          )}
+
           <p className="text-lg">{post.body}</p>
+          <p className="text-lg">{date}</p>
         </div>
       </main>
     </section>
